e02836205c0cbe4e16f8b5ad0b5ee18afe5d394f
Paul Sherwood <paul.sherwood@stfc.ac.uk>
2006-04-23 11:40:51 +0000

    introduced use of jobmanager class to allow execution under windows

--- interfaces/molden.py
+++ interfaces/molden.py
@@ -5,6 +5,7 @@ assumes a somewhat doctored version of MOLDEN
 """
 
 from objects.field import *
+from jobmanager import *
 
 class MoldenDriver:
 
@@ -28,25 +29,29 @@ class MoldenDriver:
         
         if len(planespec) == 3:
             fp.write("PLANE="+str(planespec) + " space" )
-
         if edge:
-            fp.write("EDGE="+str(edge))
-
+            fp.write(" EDGE="+str(edge))
         if mo:
             fp.write(" PSI="+str(mo))
-
         fp.write(" NPTSX="+str(npts)+" NPTSY="+str(npts)+" NPTSZ="+str(npts))
         fp.write("\n")
                  
-
         fp.write("FILE="+self.wfn+" WRBAS")
 
         fp.close()
 
         # execute MOLDEN
 
-        import os
-        os.system("/home/psh/molden4.4_hvd/molden.nox molden.dat")
+        #import os
+        #os.system("/home/psh/molden4.4_hvd/molden.nox molden.dat")
+
+        # Windows/Cygwin
+        job = jobmanager.BackgroundJob()
+        job.debug = 1
+        job.add_step(DELETE_FILE,'remove 3dgridfile',remote_filename='3dgridfile',kill_on_error=0)        
+        molden_exe = "C:/molden4.4_hvd/molden.exe"
+        job.add_step(RUN_APP,'run molden',local_command=molden_exe + " molden.dat")
+        job.run()
 
         # Load resultant field into a grid
         self.field = Field(nd=3)
@@ -54,7 +59,7 @@ class MoldenDriver:
 
 
 if __name__ == "__main__":
-    t=MoldenDriver("/home/psh/molden4.4_hvd/ex1/cyclopropaan.out")
+    t=MoldenDriver("c:\molden4.4_hvd\ex1\cyclopropaan.out")
     t.ComputePlot((1,2,3))
     
 
