bf29c6d50688f54e43fc8f3607462967b9e6592a
Jens Thomas <jens.thomas@stfc.ac.uk>
2006-06-13 11:49:58 +0000

    Small change to allow the script to be called from anywhere, not just the directory where it is sitting.

--- ccp1gui.sh
+++ ccp1gui.sh
@@ -16,4 +16,17 @@ echo
 exit 1
 fi
 
-python viewer/main.py $*
+# Work out where the base gui directory is
+if [ "${0:0:1}" = "/" ]
+then
+    # we've been called with an absolute path so just get the directory
+    guidir=`dirname $0`
+else
+    # We've been called with a relative path so work out where the script lives
+    # relvative to us, go there and print the directory name to get its full path
+    path_to_script=`dirname $0`
+    guidir=`(cd $path_to_script; pwd)`
+fi
+
+
+python $guidir/viewer/main.py $*
