9d18dd0d4a629430e5d91ae0af51ae1472264ae1
Paul Sherwood <paul.sherwood@stfc.ac.uk>
2006-09-01 16:22:57 +0000

    revise way am1 is invoked to bring into line with other computational methods (although there is actualy no options panel).

--- interfaces/am1calc.py
+++ interfaces/am1calc.py
@@ -34,54 +34,51 @@ from qm import *
 
 from objects import am1
 
+
+from interfaces.calcmon import *
+
 MENU_ENER  = "Energy"
 MENU_GRAD  = "Gradient"
 MENU_OPT   = "Geometry Optimisation"
 
 class AM1Calc(QMCalc):
     """Calculation object for the AM1 calculation
-       
-
     """
-#    def __init__(self, **kw):
-    def __init__(self, initialMolecule=None, options=None, **kw):
+    def __init__(self, **kw):
 
-        if not initialMolecule:
-            print "AM1Calc needs a molecule!"
-            return None
-        else:
-            self.initialMolecule = initialMolecule
+        print QMCalc
+        print AM1Calc
 
-        apply(QMCalc.__init__,(self,),kw)
+        QMCalc.__init__(self,**kw)
 
         self.debug = 1
-
-        self.molecules = []
         self.generator = None
         self.optstep = 0 # to count optimisation steps
-        
-        self.set_defaults( initialMolecule )
-            
-        # dictionary of options for the calculation
-        if options:
-            self.set_options( options )
-            
 
-    def set_defaults( self, molecule ):
+        self.molecules = []
+        self.AM1Energies = []
+
+    def set_defaults( self ):
         """ Set up the default values for the calculation """
-        
+        print 'set def'
+
+        self.AM1Energies = []
+        self.AM1Mol = None
+
         self.set_program('AM1')
-        self.set_title( molecule.title )
+        self.set_title('x')
         self.set_parameter('task',MENU_OPT)
         self.set_parameter('frozen_density', 1)
         self.set_parameter('fixed', -1)
         self.set_parameter('opt_method','Newton')
-
+        self.set_parameter('job_name','am1 cleanup')
+        
     def check_avail_parameters(self):
         """Check if we have parameters for all the atoms."""
 
         failed = []
-        for atom in self.initialMolecule.atom:
+        mol = self.get_input('mol_obj')
+        for atom in mol.atom:
             if not atom.symbol in am1.AM1atoms:
                 print "symbol %s not in list" % atom.symbol
                 failed.append(atom.symbol)
@@ -91,16 +88,80 @@ class AM1Calc(QMCalc):
         else:
             return None
 
+    def makejob(self,writeinput=1,graph=None):
+        """Prepare a job which will perform AM1 optimisation of a molecule
+        """
+
+        if self.debug:
+            print "building AM1 job"
+
+        #Get an editor instance to pop up tk-error widgets
+        ed = self.get_editor()
+
+        self.set_defaults()
+
+        job = jobmanager.ForegroundJob()
+
+        # Check we have parameters for this atom
+        failed = self.check_avail_parameters()
+        if failed:
+            txt = ''
+            for element in failed:
+                txt = txt+element+' '
+            if ed:
+                ed.error("Sorry, cannot run am1 optimisationas there are no parameters for the elements: %s" % txt)
+            else:
+                print "Sorry, cannot run am1 optimisationas there are no parameters for the elements: %s" % txt
+            return
+
+        self.GetModel()
+        mol_name = self.get_input("mol_name")
+        job_name = self.get_parameter("job_name")
+
+        # connect up the monitor to load structure back
+        if ed:
+            job.add_monitor(ed.monitor)
+
+        job.add_step(PYTHON_CMD,'am1 optimisation',proc=self.runAM1)
+        #job.add_tidy(self.endjob2)
+        
+        return job
+
+    def endjob(self,graph):
+        """This is executed in the slave thread when the job completes
+        successfully.
+        There should be no output from slaves unless activated from
+        using the debug_slave flag.
+        """
+        return 0,""
+
+    def endjob2(self,code=0):
+        """This function is executed in the main thread"""
+
+        if self.debug:
+            print 'running endjob2 code=',code
+
+        # load contents of listing for viewing
+        if self.debug_slave:
+            print 'endjob....'
+        job_name = self.get_parameter("job_name")
+        directory = self.get_parameter("directory")
+        file = open(directory+'/'+job_name+'.out','r')
+        self.ReadOutput(file)
+        file.close()
+
+        print 'scan output'
+
 
     def get_generator( self ):
         """Return a generator object that can be used to cycle through the
            geometry optimisation steps and return geometries.
         """
 
+        mol=self.get_input('mol_obj')
         Am1Mol = am1.Molecule()
-        
         # Create the molecule
-        for atom in self.initialMolecule.atom:
+        for atom in mol.atom:
             Am1Mol.add( atom.name, atom.symbol,atom.coord[0], atom.coord[1], atom.coord[2] )
         if self.get_parameter( 'opt_method' ) == 'Newton':
             print "Running Newton calculation"
@@ -111,7 +172,6 @@ class AM1Calc(QMCalc):
 
         return None
 
-
     def get_opt_step( self ):
         """Return a molecule and it's energy from an optimisation step"""
 
@@ -119,55 +179,203 @@ class AM1Calc(QMCalc):
             self.get_generator()
 
         try:
-            atoms, energy = self.generator.next()
+            junk = self.generator.next()
+            print 'TEST',junk
+            atoms, energy = junk
+            
+            tmp = zmatrix.Zmatrix()
+            for new in atoms:
+                atom = zmatrix.ZAtom()
+                atom.name = new.name
+                atom.symbol = new.symbol
+                atom.coord = [ new.x, new.y, new.z ]
+                tmp.atom.append( atom )
+
+            self.AM1Mol = tmp
+            self.molecules.append(tmp)
+
+            # Make the new energy available to the calcmon
+            self.AM1Energies.append(energy)
+
         except StopIteration:
             # Optimisation has completed
             return None, None
             
         self.optstep += 1
 
-        Am1Mol = zmatrix.Zmatrix()
-        for new in atoms:
-            atom = zmatrix.ZAtom()
-            atom.name = new.name
-            atom.symbol = new.symbol
-            atom.coord = [ new.x, new.y, new.z ]
-            Am1Mol.atom.append( atom )
-
-        if len(self.molecules) == 0:
-            newmol = copy.deepcopy( self.initialMolecule )
-        else:
-            newmol = copy.deepcopy( self.molecules[-1] )
-            
-        newmol.name = '%s_%d' % ( newmol.title, self.optstep )
-        newmol.import_geometry( Am1Mol )
-        
-        self.molecules.append( newmol )
-
         print "get_opt_step returning:"
         print energy
-        print newmol
+        #print newmol
+        return tmp,energy
+
+
+    def runAM1(self):
+        """ Run the AM1 Optimiser. This loops over each optimisation point updating the
+            main window with the latest structure.
+        """
+        print "Optimisation running"
+        finished = None
+        i=0
+        while not finished:
+            print "Optimisation step: %d" % i
+            newmol, energy = self.get_opt_step()
+            if newmol == None:
+                # We've finished so clean up and break out of the loop
+                print "runAM1 finished optimisation."
+                finished=1
+                self.generator = None
+                break
+            i+=1 
+        return 0,""
+
+    def get_editor_class(self):
+        return AM1CalcEd
+
+
+class AM1CalcEd(QMCalcEd):
+    """ This is not meant to provide input option editing, but provides
+    access to the Run method which is used to control job execution and
+    links to the job editor - and perhaps also calculation monitor
+    """
+    
+    def __init__(self,root,calc,graph,**kw):
+        apply(QMCalcEd.__init__, (self,root,calc,graph), kw)
+
+        # there will eventually be just one of these
+        self.calcMon = None
+
+    def Run(self,writeinput=1):
+        """Run the calculation, via the following steps
+        - ensure all widget data has been stored
+        - invoke the makejob method of the calculation itself
+          this will incorporate any
+              * file staging
+              * execution
+              * postprocessing (eg python command)
+              * tidy function (to execute once the job has finished or died).
+        - register the job with the Job manager so its status is visible in the
+          job editor
+        - show the job editor
+        - create a new thread and start execution
+        """
+        if self.master != None:
+            print 'cant run slave calculation'
+            return
+
+        # self.ReadWidgets()
+
+        # build job
+        # .. this includes making up the input deck and
+        #    scheduling the job steps
+        # the graph object is needed so that the job can include
+        # the final load of results back into the GUI
+        job = self.calc.makejob(writeinput=writeinput,graph=self.graph)
+
+
+        # create a calculation monitor window
+        if not self.calcMon:
+            self.calcMon = CalculationMonitor(self.root,command=self.calcmon_ops)
+        self.calcMon.clear()
+        self.calcMon.show()
+            
+        #self.AM1Lock = thread.allocate_lock()
+
+        # register with the job manager
+        if job:
+            self.job_editor.manager.RegisterJob(job)
+            self.job_editor.show()
+        else:
+            self.Error("Problem preparing Job - not submitted")
+            return
+
+        if self.job_thread == None:
+            pass
+        elif self.job_thread.isAlive():
+            self.Error("This calculation is running already!")
+            return
+
+        self.job_thread = JobThread(job)
+
+        try:
+            #self.CheckData()
+            self.job_thread.start()
+        except RuntimeError,e:
+            #self.message["title"] = "Error"
+            #self.message["message_text"] = str(e)
+            #self.message["iconpos"] = 'w'
+            #self.message["icon_bitmap"] = 'error'
+            #self.message["buttons"] = ("Dismiss")
+            print 'exception'
+            self.Error(str(e))
+        print 'job done'
+
+
+    def monitor(self):
+        """Transfer partially completed structure to GUI and update the graph widget
+        """
+        #print 'monitor'
+        # Update displayed structure if a new geomtry has arrived
+        if self.calc.AM1Mol:
+            mol = self.calc.get_input('mol_obj')
+            print 'UPDATE GEOM',self.calc.AM1Mol.atom[0].coord
+            mol.import_geometry( self.calc.AM1Mol )
+            if self.graph:
+                print 'UPDATE GRAPH',self.calc.AM1Mol.atom[0].coord
+                self.graph.update_from_object(mol)
+            self.calc.AM1Mol = None
+
+            # Update graph widget
+            self.calcMon.update()
+            self.calcMon.show()
+
+    def calcmon_ops(self,operation,arguments):
+        """ The operations that are passed to the calculation monitor.
+        """
+        if operation == 'newValues':
+            print "refreshing calc from calcmon"
+            # Probably don't need to lock here, as in the worst case we just get slightly
+            # out of date values
+            return self.calc.AM1Energies,None
         
-        return newmol,energy
+        elif operation == 'stop':
+            print "Stopping calc from calcmon"
+            self.stopAM1Opt()
+
 
 if __name__ == "__main__":
 
-    try:
-        myfile = sys.argv[1]
-        mol = zmatrix.Zmatrix()
-        mol.load_from_file( myfile )
-    except Exception,e:
-        print "I need a file with coordinates/a zmatrix in it!"
-        print e
-        sys.exit(1)
+    #mystery error if the following statement is included
+    #from interfaces.gamessuk import *
+    from objects.zmatrix import *
+    from jobmanager import *
+    model = Zmatrix()
+    atom = ZAtom()
+    atom.symbol = 'C'
+    atom.name = 'C'
+    model.insert_atom(0,atom)
+    atom = ZAtom()
+    atom.symbol = 'H'
+    atom.name = 'H'
+    atom.coord = [ 1.,0.,0. ]
+    model.insert_atom(1,atom)
+    atom = ZAtom()
+    atom.symbol = 'H'
+    atom.name = 'H'
+    atom.coord = [ 1.,1.,0. ]
+    model.insert_atom(1,atom)
 
-    calc = AM1Calc( initialMolecule=mol )
+    print 'x'
+    calc = AM1Calc()
+    calc.set_input('mol_obj',model)
+    #calc.set_defaults()
+    #calc.runAM1()
 
-    for i in range(2):
-        print "Iteration %d" % i
-        newmol, energy = calc.get_opt_step()
-        print 'gen.next got'
-        print 'newmol'
-        print newmol
-        print 'energy'
-        print energy
+    root=Tk()
+    if 1:
+        calc.set_input('mol_obj',model)
+        jm = JobManager()
+        je = JobEditor(root,jm)
+        calc2 = copy.deepcopy(calc)
+        vt = AM1CalcEd(root,calc,None,job_editor=je)
+        vt.Run()
+    root.mainloop()
