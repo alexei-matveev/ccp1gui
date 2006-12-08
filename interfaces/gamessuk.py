3d5fbe1c6aba1d1749de88d47445a52c0f4efed6
Paul Sherwood <paul.sherwood@stfc.ac.uk>
2006-12-08 14:57:26 +0000

    test commit (debugging cvs problems)

--- interfaces/gamessuk.py
+++ interfaces/gamessuk.py
@@ -1,7 +1,7 @@
 #
 #    This file is part of the CCP1 Graphical User Interface (ccp1gui)
 # 
-#   (C) 2002-2005 CCLRC Daresbury Laboratory
+#   (C) 2002-2006 CCLRC Daresbury Laboratory
 # 
 #   This program is free software; you can redistribute it and/or modify
 #   it under the terms of the GNU General Public License as published by
