b649753b76e83da3807a40ac4a9defb1422554c1
Paul Sherwood <paul.sherwood@stfc.ac.uk>
2007-06-21 14:07:45 +0000

    there was a problem with molpro in that the freqs were only reported to 2 places and therefor degenerate vibrations got the same label, which is not good when using the selector widget. This changes allows the set of frequencies to be built up making sure each name is unique.

--- objects/vibfreq.py
+++ objects/vibfreq.py
@@ -1,7 +1,7 @@
 #
 #    This file is part of the CCP1 Graphical User Interface (ccp1gui)
 # 
-#   (C) 2002-2005 CCLRC Daresbury Laboratory
+#   (C) 2002-2007 CCLRC Daresbury Laboratory
 # 
 #   This program is free software; you can redistribute it and/or modify
 #   it under the terms of the GNU General Public License as published by
@@ -24,14 +24,54 @@ frequencies.
 class VibFreqSet:
    def __init__(self):
       self.vibs = []
+      self.title = 'Normal Modes'
+      self.vibtitles = []
+      self.vib_count = 0
+
+   def list(self):
+      print 'Vib Freq Set - freqs:',
+      for v in self.vibs:
+         print v.title,
+
+   def add_vib(self,disp,freq=None):
+      """convenience function to add a new vibration making sure the
+      title is unique (if not, the selector widget won't work..)"""
+      v = VibFreq(self.vib_count)
+      self.vib_count = self.vib_count+1
+      self.vibs.append(v)
+      v.displacement=disp
+      if freq is not None:
+         self.set_freq(v,freq)
+      else:
+         v.title='v #'+str(self.vib_count)
+         v.freq = -1
+      return v
+
+   def set_freq(self, v, freq):
+      v.freq = freq
+      for descriminator in [ '', '_2', '_3', '_4', '_6','_7','_8']:
+         title = 'v'+str(freq)+descriminator
+         try:
+            self.vibtitles.index(title)
+         except ValueError:
+            break
+      v.title = title
+      self.vibtitles.append(title)
 
 class VibFreq:
    def __init__(self,index):
       self.index=index
-      self.freq=0.0
       self.name=None
-      self.title="v???"
       self.reference = None
+      self.freq=0.0
+      self.title="v???"
 
    def get_name(self):
       return self.title
+
+
+v = VibFreqSet()
+v1 = v.add_vib([],100)
+v2 = v.add_vib([])
+v.set_freq(v2,100)
+v.list()
