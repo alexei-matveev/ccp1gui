52961776bd83bf8ff918ccc2acf38f1a23fe29fd
Jens Thomas <jens.thomas@stfc.ac.uk>
2010-05-13 15:28:39 +0000

    Base CCP1GUI_Data object class lives here.
    All data objects should inherit from this so that any common methods (currently only GetClass) can be implemented here.

---
+++ objects/object.py
@@ -0,0 +1,12 @@
+#
+# Base class to hold any methods used by all CCP1GUI Data objects
+#
+class CCP1GUI_Data():
+    
+    def __init__(self):
+        pass
+
+    def GetClass(self):
+        """Return the last bit of the class decription as the objects class"""
+        myclass = str(self.__class__).split('.')[-1]
+        return myclass
