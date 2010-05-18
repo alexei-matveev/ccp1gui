c681721f8b132df21c2a34b36e666b9f23e80401
Jens Thomas <jens.thomas@stfc.ac.uk>
2010-05-18 17:18:06 +0000

    Added some more tests - not sure how to handle running this when different codes are/aren't present.

--- testall.py
+++ testall.py
@@ -2,13 +2,42 @@
 #
 #Script to run all the current CCP1GUI tests
 #
+import os,sys
 
+import viewer.defaults
+
+# Set paths to code binaries/scripts below:
+#
+#
+# GAMESS-UK
+#
+#os.environ['GAMESS_EXE']='/home/jmht/Documents/GAMESS-UK.gnu/bin/gamess'
+rungamess_dir='/home/jmht/Documents/GAMESS-UK.gnu/rungamess'
+os.environ['PATH']=os.environ['PATH']+os.pathsep+rungamess_dir
+#
+# ChemShell
+#
+chemsh_script_dir='/c/ccg/share/software/ChemShell/ChemShell-3.4.dev/scripts'
+viewer.defaults.defaults.set_value('chemsh_script_dir', chemsh_script_dir)
+
+
+#
 import unittest
-
 #
 # The testsuite to hold the series of tests that we will run
 #
 testsuite = unittest.TestSuite()
+
+#
+# We have just one Tk root instance to run all the tests
+# It's somewhat of a dirty hack, but we poke the tkroot instance into 
+# the module namespace of the various modules that run graphical tests
+
+#
+import Tkinter
+tkroot=Tkinter.Tk()
+#tkroot.withdraw()
+
 
 ###########################################################################
 #
@@ -43,17 +72,28 @@ import interfaces.charmm
 testsuite.addTests(interfaces.charmm.testMe())
 import interfaces.filepunch
 testsuite.addTests(interfaces.filepunch.testMe())
-import interfaces.cubereader
-testsuite.addTests(interfaces.cubereader.testMe())
-import interfaces.testgamessuk
-testsuite.addTests(interfaces.testgamessuk.testMe())
+#import interfaces.cubereader
+#testsuite.addTests(interfaces.cubereader.testMe())
 import interfaces.am1calc
 testsuite.addTests(interfaces.am1calc.testMe())
 
+# interfaces with graphical tests
+import interfaces.testgamessuk
+interfaces.testgamessuk.tkroot=tkroot
+testsuite.addTests(interfaces.testgamessuk.testMe())
+
+#import interfaces.dalton
+#interfaces.dalton.tkroot=tkroot
+#testsuite.addTests(interfaces.dalton.testMe())
+
+import interfaces.mndo
+interfaces.mndo.tkroot=tkroot
+testsuite.addTests(interfaces.mndo.testMe())
+
 # Below still to do:
-#interfaces/testmolpro.py:import unittest
-#interfaces/testmolden.py:import unittest
-#interfaces/testchemshell.py:import unittest
+#interfaces/testmolpro.py
+#interfaces/testmolden.py
+#interfaces/testchemshell.py
 
 
 ###############################################################
@@ -62,12 +102,7 @@ testsuite.addTests(interfaces.am1calc.testMe())
 #
 ###############################################################
 
-# Just have one root instance to run all the tests
-import Tkinter
-tkroot=Tkinter.Tk()
-tkroot.withdraw()
 import viewer.testVisualisers
-# Horrible, dirty hack - pass the tkroot instance to the testVisualisers module
 viewer.testVisualisers.tkroot=tkroot
 testsuite.addTests(viewer.testVisualisers.testMe())
 
